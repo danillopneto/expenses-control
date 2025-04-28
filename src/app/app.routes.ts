2821c6cc314e899c6677588010262c0338c58af1
dnetotl <dneto@thinklogic.net>
2025-04-28 13:41:23 -0300

    More features.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { CategoriesComponent } from './categories/categories.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { CategoriesResolver } from './resolvers/categories.resolver';
 import { AccountsResolver } from './resolvers/accounts.resolver';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -16,5 +17,6 @@ export const routes: Routes = [
   { path: 'expenses-list', component: ExpensesListComponent, canActivate: [authGuard], resolve: { categories: CategoriesResolver, accounts: AccountsResolver } },
   { path: 'categories', component: CategoriesComponent, canActivate: [authGuard] },
   { path: 'accounts', component: AccountsComponent, canActivate: [authGuard] },
+  { path: 'settings', component: SettingsComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CategoriesComponent } from '../categories/categories.component';
+import { AccountsComponent } from '../accounts/accounts.component';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [CommonModule, MatTabsModule, CategoriesComponent, AccountsComponent],
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent {}
